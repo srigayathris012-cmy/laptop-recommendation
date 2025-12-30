697c394234d1d153c3f0ff9d95b13461085b6773
srigayathris012-cmy <srigayathri.s012@gmail.com>
2025-12-30 21:34:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ for _, row in rec_df.iterrows():
                 color:white;                /* white text for readability */
                 box-shadow: 2px 2px 10px rgba(0,0,0,0.5);
             ">
-                <h3 style="color:white;">{row['Model']}</h3>
+                <h3 style="color:black;">{row['Model']}</h3>
                 <p><b>Price:</b> ₹{row.get('Price', 'N/A')}</p>
                 <p><b>RAM:</b> {row.get('Ram', 'N/A')}</p>
                 <p><b>SSD:</b> {row.get('SSD', 'N/A')}</p>
